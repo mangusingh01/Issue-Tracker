c286f789e8228da1b7426d1b4d35e292ccbdec53
Mangu Singh <mangusingh@Mangus-MacBook-Pro.local>
2025-07-17 02:16:51 +0530

    issue assign API workaround

---
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { User } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@/app/components";
+
+const AssigneeSelect = () => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3,
+  });
+
+  if (isLoading) return <Skeleton />;
+
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default AssigneeSelect;
