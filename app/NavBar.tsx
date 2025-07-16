d76adf693d576e482ad708a4c20028d3639b4f37
Mangu Singh <mangusingh@Mangus-MacBook-Pro.local>
2025-07-16 17:45:31 +0530

    style the active link in nav bar

---
+++ app/NavBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={classnames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
